9587a9091aa600339d7caa757e6ac3675a5b45d7
tzh <Tangzhongham@gmail.com>
2023-05-17 21:29:16 +0800

    Initial Commit

---
+++ Scorer Watch App/ScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorerApp.swift
+//  Scorer Watch App
+//
+//  Created by 唐某某 on 2023/5/17.
+//
+
+import SwiftUI
+
+@main
+struct Scorer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scorer/ScorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorerApp.swift
+//  Scorer
+//
+//  Created by 唐某某 on 2023/5/17.
+//
+
+import SwiftUI
+
+@main
+struct ScorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
